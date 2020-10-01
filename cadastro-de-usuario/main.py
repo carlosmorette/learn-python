22129ff9f94a817aa77ba499586a19aa6426374d
CarlosMorette <carlos.morette@outlook.com>
2020-10-01 11:53:05 -0300

    Backup mini projeto e nova ferramenta

--- cadastro-de-usuario/main.py
+++ cadastro-de-usuario/main.py
@@ -8,7 +8,7 @@ class User(tornado.web.RequestHandler):
 
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header('Access-Control-Allow-Methods', 'POST')
 
     def save_user(self):
@@ -16,13 +16,17 @@ class User(tornado.web.RequestHandler):
             print(json.load(users))
 
     def post(self):
-        body = tornado.escape.json_decode(self.request.body)
-        print(body)
+        # body = tornado.escape.json_decode(self.request.body)
+        self.write({
+            "name": "asdsadad"
+        })
 
 def make_app():
     return tornado.web.Application([
         (r"/", User),
-    ],autoreload=True)
+    ],
+        autoreload=True
+    )
 
 if __name__ == "__main__":
     app = make_app()
